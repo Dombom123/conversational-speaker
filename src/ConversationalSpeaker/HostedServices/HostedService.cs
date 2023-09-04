da42cdb18ffb80de39bb0f021161b26e850129ad
dombom123 <dominik.boehm@gmx.de>
2023-09-04 14:56:34 +0200

    testig async startup

--- src/ConversationalSpeaker/HostedServices/HostedService.cs
+++ src/ConversationalSpeaker/HostedServices/HostedService.cs
@@ -166,11 +166,19 @@ namespace ConversationalSpeaker
             }
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
+            // Start the ReadCommandsAsync task to continuously read from the console.
             _ = ReadCommandsAsync(_cancelToken.Token);
+
+            // Automatically execute the greet logic on startup.
+            var randomGreeting = _greetings[_random.Next(_greetings.Count)];
+            await _semanticKernel.RunAsync(randomGreeting, _speechSkill["Speak"]);
+
+            // Start the ExecuteAsync task for button press listening.
             _ = ExecuteAsync(_cancelToken.Token);
-            return Task.CompletedTask;
+            
+            return;
         }
 
 
