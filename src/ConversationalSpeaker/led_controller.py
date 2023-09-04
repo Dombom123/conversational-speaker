6bce13bd56c61357ccf22eaea8c160a766c26a1c
dombom123 <dominik.boehm@gmx.de>
2023-09-04 14:38:24 +0200

    reset led script

--- src/ConversationalSpeaker/led_controller.py
+++ src/ConversationalSpeaker/led_controller.py
@@ -1,28 +1,48 @@
 import time
 from rpi_ws281x import PixelStrip, Color
 import argparse
-import threading
 
 # LED strip configuration:
 LED_COUNT = 20
 LED_PIN = 12
 LED_FREQ_HZ = 800000
 LED_DMA = 10
-LED_BRIGHTNESS = 120
+LED_BRIGHTNESS = 100
 LED_INVERT = False
 LED_CHANNEL = 0
 
-stop_event = threading.Event()
-
 
 def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
         strip.show()
         time.sleep(wait_ms / 1000.0)
 
 
+def theaterChase(strip, color, wait_ms=50):
+    """Movie theater light style chaser animation."""
+    for q in range(3):
+        for i in range(0, strip.numPixels(), 3):
+            strip.setPixelColor(i + q, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+        for i in range(0, strip.numPixels(), 3):
+            strip.setPixelColor(i + q, 0)
+
+
+def rainbowCycle(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
 def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
     if pos < 85:
         return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
@@ -32,48 +52,54 @@ def wheel(pos):
         pos -= 170
         return Color(0, pos * 3, 255 - pos * 3)
 
+def colorPulse(strip, color, pulse_iterations=3, wait_ms=50):
+    """Pulse a color in and out."""
+    for j in range(pulse_iterations):
+        # Fade in
+        for k in range(0, 256, 10):
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, Color(int(color[0] * k / 255), int(color[1] * k / 255), int(color[2] * k / 255)))
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+        # Fade out
+        for k in range(255, -1, -10):
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, Color(int(color[0] * k / 255), int(color[1] * k / 255), int(color[2] * k / 255)))
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+
 
 def set_color(strip, color):
+    """Set the entire strip to a single color."""
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
     strip.show()
 
 
 def idle_state(strip):
-    while not stop_event.is_set():
-        colorWipe(strip, Color(255, 255, 255), 500)
+    """Idle state animation: White wipe."""
+    colorWipe(strip, Color(255, 255, 255))
 
 
 def listening_state(strip):
-    while not stop_event.is_set():
-        set_color(strip, Color(0, 255, 0))
-        time.sleep(0.5)
+    """Listening state: Bright green color."""
+    set_color(strip, Color(0, 255, 0))
 
 
 def thinking_state(strip):
-    while not stop_event.is_set():
-        set_color(strip, Color(0, 0, 255))
-        time.sleep(0.5)
+    """Thinking state animation: Green color wipe loop."""
+    set_color(strip, Color(0, 0, 255))
+
 
 
 def responding_state(strip):
-    while not stop_event.is_set():
-        set_color(strip, Color(255, 0, 0))
-        time.sleep(0.5)
-
+    """Responding state animation: Pulsing light in red."""
+    set_color(strip, Color(255, 0, 0))
 
 def clear_state(strip):
+    """Turn off"""
     set_color(strip, Color(0, 0, 0))
-
-
-def start_animation(action_func, strip):
-    global stop_event
-    stop_event.set()
-    time.sleep(0.5)
-    stop_event.clear()
-    t = threading.Thread(target=action_func, args=(strip,))
-    t.start()
-    return t
 
 
 if __name__ == '__main__':
@@ -85,24 +111,15 @@ if __name__ == '__main__':
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
 
-    animation_thread = None
     if args.action == 'idle':
-        animation_thread = start_animation(idle_state, strip)
+        idle_state(strip)
     elif args.action == 'listening':
-        animation_thread = start_animation(listening_state, strip)
+        listening_state(strip)
     elif args.action == 'thinking':
-        animation_thread = start_animation(thinking_state, strip)
+        thinking_state(strip)
     elif args.action == 'responding':
-        animation_thread = start_animation(responding_state, strip)
-    elif args.action == 'clear':
-        clear_state(strip)
-
-    if animation_thread:
-        try:
-            animation_thread.join()
-        except KeyboardInterrupt:
-            stop_event.set()
-            animation_thread.join()
+        responding_state(strip)
 
     if args.clear:
         colorWipe(strip, Color(0, 0, 0), 10)
+
